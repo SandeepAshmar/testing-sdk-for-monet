3835d23f70080e095c00ceb22d1a84fdbf8bf4dc
sandeep <sandeep@ashmar.in>
2018-11-10 13:46:12 +0530

    btn

--- mylibrary/src/main/java/com/monet/mylibrary/DebugActivity.java
+++ mylibrary/src/main/java/com/monet/mylibrary/DebugActivity.java
@@ -6,25 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class DebugActivity extends AppCompatActivity {
 
     private static final String TAG = "SUPER_AWESOME_APP";
 
-    private Button btn1, btn2, btn3, btn4;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
-
-        btn1 = findViewById(R.id.btn_one);
-        btn2 = findViewById(R.id.btn_two);
-        btn3 = findViewById(R.id.btn_three);
-        btn4 = findViewById(R.id.btn_four);
-
     }
 
     public static void d(String message) {
@@ -43,15 +34,15 @@ public class DebugActivity extends AppCompatActivity {
         Toast.makeText(view.getContext(), "btn 1", Toast.LENGTH_SHORT).show();
     }
 
-    public void btnSecoundClick(View view){
+    public static void btnSecoundClick(View view){
         Toast.makeText(view.getContext(), "btn 2", Toast.LENGTH_SHORT).show();
     }
 
-    public void btnThirdClick(View view){
+    public static void btnThirdClick(View view){
         Toast.makeText(view.getContext(), "btn 3", Toast.LENGTH_SHORT).show();
     }
 
-    public void btnForthClick(View view){
+    public static void btnForthClick(View view){
         Toast.makeText(view.getContext(), "btn 4", Toast.LENGTH_SHORT).show();
     }
 }
