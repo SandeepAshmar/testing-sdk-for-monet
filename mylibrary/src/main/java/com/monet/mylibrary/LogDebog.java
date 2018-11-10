a56132e1a01a0c4046c7b11510b6077c6d6453ff
sandeep <sandeep@ashmar.in>
2018-11-10 11:04:46 +0530

    first change

--- mylibrary/src/main/java/com/monet/mylibrary/LogDebog.java
+++ mylibrary/src/main/java/com/monet/mylibrary/LogDebog.java
@@ -1,13 +1,21 @@
 package com.monet.mylibrary;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 abstract public class LogDebog {
 
     private static final String TAG = "SUPER_AWESOME_APP";
 
-    public static void d(String message){
+    Context context;
+
+    public static void d(String message) {
         Log.d(TAG, message);
     }
-    
+
+    public void Toast(String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
 }
