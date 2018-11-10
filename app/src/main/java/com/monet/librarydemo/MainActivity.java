abc455067fbd274ea56d0c3cd7df24ddd5e147ba
sandeep <sandeep@ashmar.in>
2018-11-10 14:23:08 +0530

    btn

--- app/src/main/java/com/monet/librarydemo/MainActivity.java
+++ app/src/main/java/com/monet/librarydemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.monet.librarydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void btnFirstClick(View view){
+        Toast.makeText(view.getContext(), "btn 1", Toast.LENGTH_SHORT).show();
+    }
+
+    public static void btnSecoundClick(View view){
+        Toast.makeText(view.getContext(), "btn 2", Toast.LENGTH_SHORT).show();
+    }
+
+    public static void btnThirdClick(View view){
+        Toast.makeText(view.getContext(), "btn 3", Toast.LENGTH_SHORT).show();
+    }
+
+    public static void btnForthClick(View view){
+        Toast.makeText(view.getContext(), "btn 4", Toast.LENGTH_SHORT).show();
     }
 }
