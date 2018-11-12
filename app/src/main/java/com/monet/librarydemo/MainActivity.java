82d88670e0b5e4a246bd8267824e9fcc88d3e82f
sandeep <sandeep@ashmar.in>
2018-11-12 13:46:59 +0530

    new comit 2

--- app/src/main/java/com/monet/librarydemo/MainActivity.java
+++ app/src/main/java/com/monet/librarydemo/MainActivity.java
@@ -12,20 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public static void btnFirstClick(View view){
-        Toast.makeText(view.getContext(), "btn 1", Toast.LENGTH_SHORT).show();
-    }
-
-    public static void btnSecoundClick(View view){
-        Toast.makeText(view.getContext(), "btn 2", Toast.LENGTH_SHORT).show();
-    }
-
-    public static void btnThirdClick(View view){
-        Toast.makeText(view.getContext(), "btn 3", Toast.LENGTH_SHORT).show();
-    }
-
-    public static void btnForthClick(View view){
-        Toast.makeText(view.getContext(), "btn 4", Toast.LENGTH_SHORT).show();
-    }
 }
