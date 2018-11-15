bce56802632436fed94508b282b5948af4577713
sandeep <sandeep@ashmar.in>
2018-11-15 09:43:10 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/connection/ApiInterface.java
+++ mylibrary/src/main/java/com/monet/mylibrary/connection/ApiInterface.java
@@ -8,6 +8,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ApiInterface {
 
@@ -16,6 +17,6 @@ public interface ApiInterface {
     Call<GetCampDetails_Pojo> getCampDetails(@Header("Authorization") String token, @Path("cam_id") String cam_id);
 
     @Headers("Content-Type: application/json")
-    @GET("sdk/1420/11111")
-    Call<QuestionModelPojo> getSdk(@Path("cam_id") String cam_id, @Path("user_id") String userId);
+    @GET("sdk/{cam_id}/{user_id}")
+    Call<QuestionModelPojo> getSdk(@Query("cam_id") String cam_id, @Query("user_id") String userId);
 }
