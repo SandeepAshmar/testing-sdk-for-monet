81fbb8c4b78b485e7c421dd8b2558560386d4ad8
sandeep <sandeep@ashmar.in>
2018-11-15 11:18:24 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/connection/ApiInterface.java
+++ mylibrary/src/main/java/com/monet/mylibrary/connection/ApiInterface.java
@@ -17,6 +17,6 @@ public interface ApiInterface {
     Call<GetCampDetails_Pojo> getCampDetails(@Header("Authorization") String token, @Path("cam_id") String cam_id);
 
     @Headers("Content-Type: application/json")
-    @GET("sdk/{cam_id/userId}")
-    Call<QuestionModelPojo> getSdk(@Path("cam_id/userId") String cam_id);
+    @GET("sdk/{cam_id}")
+    Call<QuestionModelPojo> getSdk(@Path("cam_id") String cam_id);
 }
