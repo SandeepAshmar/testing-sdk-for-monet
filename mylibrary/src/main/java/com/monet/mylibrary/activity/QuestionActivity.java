340b8a3dc393e33db44cce75738c29a43558f920
sandeep <sandeep@ashmar.in>
2018-11-16 10:28:55 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/activity/QuestionActivity.java
+++ mylibrary/src/main/java/com/monet/mylibrary/activity/QuestionActivity.java
@@ -2,19 +2,24 @@ package com.monet.mylibrary.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.monet.mylibrary.R;
 
 public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RelativeLayout rl_quesQCard;
-    private LinearLayout ll_quesQCardBtn, ll_quesNextBtn;
+    private LinearLayout ll_quesQCardBtn, ll_quesNextBtn, ll_question, grid_linearLayout;
     private Button btn_quesNext, btn_quesqa_exit, btn_quesqa_proceed;
-
+    private RecyclerView rv_question;
+    private TextView tv_questionNo, tv_question, tv_questionSelect;
+    private EditText edt_questionType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,21 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         rl_quesQCard = findViewById(R.id.rl_quesQCard);
         ll_quesQCardBtn = findViewById(R.id.ll_quesQCardBtn);
         ll_quesNextBtn = findViewById(R.id.ll_quesNextBtn);
+        ll_question = findViewById(R.id.ll_question);
+        grid_linearLayout = findViewById(R.id.grid_linearLayout);
         btn_quesNext = findViewById(R.id.btn_quesNext);
         btn_quesqa_proceed = findViewById(R.id.btn_quesqa_proceed);
+        rv_question = findViewById(R.id.rv_question);
+        tv_questionNo = findViewById(R.id.tv_questionNo);
+        tv_question = findViewById(R.id.tv_question);
+        tv_questionSelect = findViewById(R.id.tv_questionSelect);
+        edt_questionType = findViewById(R.id.edt_questionType);
         btn_quesqa_exit = findViewById(R.id.btn_quesqa_exit);
         btn_quesNext.setEnabled(false);
+
+        btn_quesqa_proceed.setOnClickListener(this);
+        btn_quesqa_exit.setOnClickListener(this);
+        btn_quesNext.setOnClickListener(this);
 
     }
 
@@ -40,7 +56,7 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
             ll_quesNextBtn.setVisibility(View.VISIBLE);
             ll_quesQCardBtn.setVisibility(View.GONE);
             rl_quesQCard.setVisibility(View.GONE);
-
+            ll_question.setVisibility(View.VISIBLE);
         }
     }
 }
