9d8e7347bf6364badd991a0d43e0085984864862
sandeep <sandeep@ashmar.in>
2018-11-21 16:04:28 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/utils/SdkConstant.java
+++ mylibrary/src/main/java/com/monet/mylibrary/utils/SdkConstant.java
@@ -4,7 +4,7 @@ import java.security.PublicKey;
 
 public class SdkConstant {
 
-    public static final String YOUTUTBE_KEY = "AIzaSyCyU-04x22s5M2Ww0LQNtc4VbnHkN8T8cI";
+    public static final String YOUTUTBE_KEY = "AIzaSyD-4LqC1Jhs6N-3fLkCrT0QDHV20OCO1So";
     public static final String RTMP_URL = "rtmp://54.149.48.63/Monet_Android/";
     public static final String RTMP_USERNAME = "admin";
     public static final String RTMP_PASSWORD = "password";
