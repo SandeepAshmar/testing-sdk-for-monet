6844c2fb98aceb8ead35a361f30fa41e718d2d42
sandeep <sandeep@ashmar.in>
2018-11-21 14:02:01 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/activity/EmotionScreen.java
+++ mylibrary/src/main/java/com/monet/mylibrary/activity/EmotionScreen.java
@@ -3,6 +3,8 @@ package com.monet.mylibrary.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.monet.mylibrary.R;
+
 public class EmotionScreen extends AppCompatActivity {
 
     @Override
