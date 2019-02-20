9542ba20f9eb4d3c5a78305dd66e08de0323ba3b
sandeep <sandeep@ashmar.in>
2019-02-20 10:16:20 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/activity/LandingPage.java
+++ mylibrary/src/main/java/com/monet/mylibrary/activity/LandingPage.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.signature.StringSignature;
 import com.monet.mylibrary.R;
 import com.monet.mylibrary.connection.ApiInterface;
 import com.monet.mylibrary.connection.BaseUrl;
@@ -170,7 +169,6 @@ public class LandingPage extends AppCompatActivity {
 
                         if (response.body().getResponse().get(0).getC_thumb_url() != null) {
                             Glide.with(activity).load(response.body().getResponse().get(0).getC_thumb_url())
-                                    .signature(new StringSignature(String.valueOf(System.currentTimeMillis())))
                                     .into(img_currentShows);
                         }else{
                             img_currentShows.setImageResource(R.drawable.ic_imagenotavailable);
