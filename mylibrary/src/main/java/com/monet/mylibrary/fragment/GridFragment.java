6e30ee13c03394737ba59c961ed9842c778239fa
sandeep <sandeep@ashmar.in>
2019-02-22 15:42:40 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/fragment/GridFragment.java
+++ mylibrary/src/main/java/com/monet/mylibrary/fragment/GridFragment.java
@@ -8,8 +8,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.monet.mylibrary.R;
+import com.monet.mylibrary.adapter.GridOptionAdapter;
+import com.monet.mylibrary.listner.RadioClickListner;
+import com.monet.mylibrary.model.question.SdkGrid;
+
+import java.util.ArrayList;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import static com.monet.mylibrary.activity.LandingPage.postQuestions;
 import static com.monet.mylibrary.activity.LandingPage.preQuestions;
@@ -22,6 +29,15 @@ public class GridFragment extends Fragment {
     private int optionPosition = 0, questionNo = 0;
     private String survayType = "";
     private TextView tv_gridOption, tv_gridCount;
+    private RecyclerView rv_grid;
+    private GridOptionAdapter gridOptionAdapter;
+
+    private RadioClickListner radioClickListner = new RadioClickListner() {
+        @Override
+        public void onItemClick(View view, int position, String quesId, String ansId) {
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,16 +53,26 @@ public class GridFragment extends Fragment {
     private View initView(View view){
         tv_gridCount = view.findViewById(R.id.tv_gridCount);
         tv_gridOption = view.findViewById(R.id.tv_gridOption);
+        rv_grid = view.findViewById(R.id.rv_grid);
+        rv_grid.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        tv_gridCount.setText("Question"+(optionPosition+1));
+        tv_gridCount.setText("Question "+(optionPosition+1));
 
         if(survayType.equalsIgnoreCase("pre")){
             tv_gridOption.setText(preQuestions.get(questionNo).getOptions().get(optionPosition).getOption_value());
+            setAdapter(preQuestions.get(questionNo).getOptions().get(optionPosition).getGrid());
         }else{
             tv_gridOption.setText(postQuestions.get(questionNo).getOptions().get(optionPosition).getOption_value());
+            setAdapter(postQuestions.get(questionNo).getOptions().get(optionPosition).getGrid());
         }
 
         return view;
+    }
+
+    private void setAdapter(ArrayList<SdkGrid> sdkGrid) {
+        gridOptionAdapter = new GridOptionAdapter(getActivity(), radioClickListner, sdkGrid);
+        rv_grid.setAdapter(gridOptionAdapter);
+        gridOptionAdapter.notifyDataSetChanged();
     }
 
 }
