b5d195bf919dba6e54470a9bc7178156a5b91870
monet-andriod2 <divya.prakash@ashmar.in>
2019-02-23 12:20:03 +0530

    Some Changes

--- mylibrary/src/main/java/com/monet/mylibrary/visionCamera/CameraSourcePreview.java
+++ mylibrary/src/main/java/com/monet/mylibrary/visionCamera/CameraSourcePreview.java
@@ -161,7 +161,8 @@ public class CameraSourcePreview extends ViewGroup {
 
         // If height is too tall using fit width, does fit height instead.
         if (childHeight > layoutHeight) {
-            childHeight = layoutHeight;
+//            childHeight = layoutHeight;
+            childHeight = 50;
 //            childWidth = (int)(((float) layoutHeight / (float) height) * width);
             childWidth = 50;
         }
