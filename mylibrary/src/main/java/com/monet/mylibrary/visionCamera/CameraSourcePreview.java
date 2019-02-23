bf18a021fc2acbe3f7feb6b1d798a1b076794971
monet-andriod2 <divya.prakash@ashmar.in>
2019-02-23 12:31:05 +0530

    Some Changes

--- mylibrary/src/main/java/com/monet/mylibrary/visionCamera/CameraSourcePreview.java
+++ mylibrary/src/main/java/com/monet/mylibrary/visionCamera/CameraSourcePreview.java
@@ -135,8 +135,10 @@ public class CameraSourcePreview extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        int width = 320;
-        int height = 240;
+//        int width = 320;
+        int width = 640;
+//        int height = 240;
+        int height = 480;
         if (mCameraSource != null) {
             Size size = mCameraSource.getPreviewSize();
             if (size != null) {
