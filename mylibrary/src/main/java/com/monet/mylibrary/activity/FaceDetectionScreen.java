9443be19002ff13ac8bfba76a805a2126ccc4fff
monet-andriod2 <divya.prakash@ashmar.in>
2019-02-23 17:44:51 +0530

    Some changes

--- mylibrary/src/main/java/com/monet/mylibrary/activity/FaceDetectionScreen.java
+++ mylibrary/src/main/java/com/monet/mylibrary/activity/FaceDetectionScreen.java
@@ -144,7 +144,7 @@ public class FaceDetectionScreen extends AppCompatActivity {
             mOverlay.add(mFaceGraphic);
             mFaceGraphic.updateFace(face);
             setFrameGreen();
-          //  Log.d(TAG, "face detected...");
+            Log.d(TAG, "face detected..." + detectionResults);
         }
 
         @Override
