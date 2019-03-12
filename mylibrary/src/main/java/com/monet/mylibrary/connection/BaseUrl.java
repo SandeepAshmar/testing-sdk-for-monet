abe34d96cb59bde6730a0e1e474aeb411fda0e54
sandeep <sandeep@ashmar.in>
2019-03-12 11:06:36 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/connection/BaseUrl.java
+++ mylibrary/src/main/java/com/monet/mylibrary/connection/BaseUrl.java
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class BaseUrl {
 
 //    public static final String BASE_URL = "https://dev.monetrewards.com/appSandbox/api/";
-    public static final String BASE_URL = "http://192.168.1.205:8002/api/";
+//    public static final String BASE_URL = "http://192.168.1.205:8002/api/";
+    public static final String BASE_URL = "https://dev.monetrewards.com/MonetSDK/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
