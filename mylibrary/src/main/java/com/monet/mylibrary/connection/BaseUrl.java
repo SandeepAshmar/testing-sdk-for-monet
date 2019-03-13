6147cfa65e14e28f6f7412b9b817196f01ca59e3
sandeep <sandeep@ashmar.in>
2019-03-13 14:29:55 +0530

    some changes

--- mylibrary/src/main/java/com/monet/mylibrary/connection/BaseUrl.java
+++ mylibrary/src/main/java/com/monet/mylibrary/connection/BaseUrl.java
@@ -1,5 +1,7 @@
 package com.monet.mylibrary.connection;
 
+import com.monet.mylibrary.BuildConfig;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -13,7 +15,7 @@ public class BaseUrl {
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(BuildConfig.hostAPI)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
